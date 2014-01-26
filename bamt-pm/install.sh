563e77df4a007466c273e797a3a529270ba95f3e
Lily Star <lily@disorg.net>
2014-01-25 22:32:26 -0800

    improved install.sh

--- bamt-pm/install.sh
+++ bamt-pm/install.sh
@@ -4,19 +4,27 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-echo -e "This script will install the IFMI web interface upgrade to BAMT.\n"\
-     "Are you sure you want to do this? y/n"
-read YN
-if [ $YN == y ]; then
+echo "This script will install the IFMI web interface upgrade to BAMT."
+read -p "Are you sure?(y/n)" input
+shopt -s nocasematch
+case "$input" in
+  y|Y|Yes)
   if [ -e /var/www/favicon.ico.bamt ]; then
-    cp /var/www/bamt/status.css /var/www/bamt/status.css.back
-    cp status.css /var/www/bamt/
-    cp /usr/lib/cgi-bin/status.pl /usr/lib/cgi-bin/status.pl.back
-    cp status.pl /usr/lib/cgi-bin/
-    cp /usr/lib/cgi-bin/poolmanage.pl /usr/lib/cgi-bin/poolmanage.pl.back
-    cp poolmanage.pl /usr/lib/cgi-bin/
-    cp /opt/bamt/common.pl /opt/bamt/common.pl.back
-    cp common.pl /opt/bamt/
+    read -p  "It looks like this has been installed before. Continue?(y/n)" overwrite
+    shopt -s nocasematch
+    case "$overwrite" in
+      y|Y|Yes)
+      cp /var/www/bamt/status.css /var/www/bamt/status.css.back
+      cp status.css /var/www/bamt/
+      cp /usr/lib/cgi-bin/status.pl /usr/lib/cgi-bin/status.pl.back
+      cp status.pl /usr/lib/cgi-bin/
+      cp /usr/lib/cgi-bin/poolmanage.pl /usr/lib/cgi-bin/poolmanage.pl.back
+      cp poolmanage.pl /usr/lib/cgi-bin/
+      cp /opt/bamt/common.pl /opt/bamt/common.pl.back
+      cp common.pl /opt/bamt/
+      echo "Done!";;
+      * ) echo "installation exited";;
+    esac
   else
     cp /var/www/favicon.ico /var/www/favicon.ico.bamt
     cp favicon.ico /var/www/
@@ -29,7 +37,7 @@ if [ $YN == y ]; then
     cp poolmanage.pl /usr/lib/cgi-bin/
     cp /opt/bamt/common.pl /opt/bamt/common.pl.bamt
     cp common.pl /opt/bamt/
-  fi
-else 
- exit 1
-fi
+    echo "Done! Please read the README and edit files as required. Thank you for flying IFMI!"
+  fi ;;
+  * ) echo "installation exited";;
+esac
