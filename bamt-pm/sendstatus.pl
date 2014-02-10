57548ac4b96c57013867bb122aabe5a7bd537e6c
Lily Star <lily@disorg.net>
2014-02-09 19:49:44 -0800

    dont display stale data on missed update. css fix for hashrate.

--- bamt-pm/sendstatus.pl
+++ bamt-pm/sendstatus.pl
@@ -24,28 +24,24 @@ sub bcastStatus
  my $ts = ${$conf}{settings}{miner_id} . '|' . ${$conf}{settings}{miner_loc};
 
  my @gpus = &getGPUData(false);
-
  for ($k = 0;$k < @gpus;$k++)
  {
   $ts .= "|$k:" . encode_json $gpus[$k];
  }
 
- my @pools = &getCGMinerPools(1);
-
+ my @pools;  my @pools = &getCGMinerPools(1);
  for ($p = 0;$p < @pools;$p++)
  {
   $ts .= "|$p pool:" . encode_json $pools[$p];
  }
 
- my @summary = &getCGMinerSummary;
-
+ my @summary; my @summary = &getCGMinerSummary;
  for ($s = 0;$s < @summary;$s++)
  {
   $ts .= "|$s sum:" . encode_json $summary[$s];
  }
 
- my @version = &getCGMinerVersion;
-
+ my @version; my @version = &getCGMinerVersion;
  for ($v = 0;$v < @version;$v++)
  {
   $ts .= "|$v ver:" . encode_json $version[$v];
@@ -57,7 +53,6 @@ sub bcastStatus
  {
   $port = ${$conf}{settings}{status_port};
  }
-
 
  my $socket = IO::Socket::INET->new(Broadcast => 1, Blocking => 1, ReuseAddr => 1, Type => SOCK_DGRAM, Proto => 'udp', PeerPort => $port, LocalPort => 0, PeerAddr => inet_ntoa(INADDR_BROADCAST));
  
