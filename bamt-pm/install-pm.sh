5ed5350f885e3176f8acf3f36b0ed1c677bc0f34
Lily Star <lily@disorg.net>
2014-02-18 19:19:01 -0800

    take out first execution now that crontab is fixed.

--- bamt-pm/install-pm.sh
+++ bamt-pm/install-pm.sh
@@ -39,7 +39,6 @@ case "$input" in
       cp /opt/bamt/common.pl /opt/bamt/common.pl.back
       cp common.pl /opt/bamt/
       cp pmgraph.pl /opt/ifmi/rrdtool
-      /opt/ifmi/rrdtool/pmgraph.pl
       if ! grep -q  "pmgraph" "/etc/crontab" ; then
         echo -e "*/5 * * * * root /opt/ifmi/rrdtool/pmgraph.pl\n" >> /etc/crontab
       fi   
